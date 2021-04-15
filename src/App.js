e4ce8c9c99b80c7422cdb8cdb98af93f60ef9465
ericcapiz <ericcapiz@gmail.com>
2021-04-15 14:53:06 -0500

    commerce store setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {useState, useEffect} from 'react';
 import './App.css';
+import {commerce} from './lib/commerce';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+    setProducts(data);
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  },[])
+
+  console.log(products);
   return (
     <div>
       <Navbar />
