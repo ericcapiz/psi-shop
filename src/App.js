01a708029c0ed78b1fd9b12d433515b31183d2ea
ericcapiz <ericcapiz@gmail.com>
2021-04-15 17:31:19 -0500

    can get items passed to cart component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import {useState, useEffect} from 'react';
-import './App.css';
 import {commerce} from './lib/commerce';
 import Navbar from './components/Navbar/Navbar';
 import Products from './components/Products/Products';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
+  
   const fetchProducts = async () => {
     const {data} = await commerce.products.list();
     setProducts(data);
   }
+  
+  const fetchCart = async() =>{
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async(productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart)
+  }
 
   useEffect(()=>{
     fetchProducts();
+    fetchCart();
   },[]);
+
+  console.log(cart);
 
   return (
     <div className="app">
-      <Navbar />
-     <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+     {/* <Products onAddToCart={handleAddToCart} products={products} /> */}
+     <Cart cart={cart} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import useStyles from './styles';
 
 
 
-const Navbar = () => {
+const Navbar = ({totalItems}) => {
     const classes = useStyles();
     return (
         <>
@@ -19,7 +19,7 @@ const Navbar = () => {
                   <div className={classes.grow} />
                   <div className={classes.button}>
                     <IconButton color="inherit" aria-label="Show Cart Items">
-                        <Badge badgeContent={2} color="secondary">
+                        <Badge badgeContent={totalItems} color="secondary">
                             <ShoppingCart className={classes.shop}/>
                         </Badge>
                     </IconButton>
